a54439632494b93034dd59cfa51829b31a370313
b0red <mr.akita@gmail.com>
2014-03-11 12:53:43 +0100

    Added backup/restore file

---
+++ b4_system_upgrade.sh
@@ -0,0 +1,44 @@
+#!/opt/bin/bash
+# Creates copys and symlinks of files that changes after a system update
+#
+# Created 2014-03
+
+clear; echo; echo "Do you want to [B]ackup or [R]estore the files?"
+
+# Settings
+BU_PATH="/volume1/my_scripts"
+BU_DIR=".UpdateBackups"
+
+
+read bro
+case $bro in 
+	[bB] )
+	# Backup
+	echo "Backing up files..."
+	echo "Creating backup filder..."
+	mkdir $BU_PATH/$BU_DIR
+	echo " $BU_PATH/$BU_DIR created!"
+	cp /etc/crontab $BU_PATH/$BU_DIR/crontab 
+	cp /root/.profile $BU_PATH/$BU_DIR/.profile
+	##cp /root/.ssh $BU_PATH/$BU_DIR/.ssh/
+	cp /root/.tmux.conf $BU_PATH/$BU_DIR/.tmux.conf
+	echo "Files copied!"
+	;;
+
+	[rR] )
+	# Restore
+	echo "Restoring files..."
+	mv $BU_PATH/$BU_DIR/crontab /etc/crontab; chmod 755
+	mv $BU_PATH/$BU_DIR/.profile /root/.profile
+	mv $BU_PATH/$BU_DIR /root/.tmux.conf
+	echo "Files restored!"
+	echo "Deleting folder: $BU_PATH/$BU_DIR/"
+	rm -rf $BU_PATH/$BU_DIR/; echo "Done!"
+	;;
+
+        *) 
+	echo "Invalid input"
+        echo "Usage: $0 [(B)ackup|(R)estore]"
+    ;;
+esac
+
