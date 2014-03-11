07927aa15bdbe30545d271e5b4bebd7cf0773ff7
b0red <mr.akita@gmail.com>
2014-03-11 14:15:49 +0100

    more cleanup

--- filelist.sh
+++ filelist.sh
@@ -5,35 +5,25 @@
 
 # Source variables
 source email_variables.sh
+source variables.sh
 
-# Variables
+# Debug 
+#echo os:$OS IP: $IP
+#echo "$SUBJECT: $NODE/$IP" $FIL $EMAIL_G $EMAIL_P
+
+
+# Internal variables
 SUBJECT="Filmlista"
 DATE=$(date +%Y%m%d)
 touch /tmp/$DATE-filmer.txt
 FIL=/tmp/$DATE-filmer.txt
 
-# Get OS & other stuff
-OS=`uname`
-NODE=`uname -n`
-
-# Store IP
-case $OS in
-   Linux) IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;;
-   FreeBSD|OpenBSD) IP=`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
-   SunOS) IP=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{ print $2} '` ;;
-   *) IP="Unknown";;
-esac
-
-#Delete old file
+# Delete old file
 find /tmp/*-filmer.txt -type f -mtime +7 -exec rm -f {} \;
 
 # Do the listing and mail it
 find /volume2/Disk2 -type f \( -iname "*.avi" -o -iname "*.m*v" -o -iname "*.iso" -o -iname "*.mp*" \) | sort -do /tmp/$DATE-filmer.txt
 echo "$SUBJECT" | /opt/bin/nail -s "$SUBJECT: $NODE/$IP" -a $FIL $EMAIL_P
-
-# Debug 
-# echo os:$OS IP: $IP
-# echo "$SUBJECT: $NODE/$IP" $FIL $EMAIL_G $EMAIL_P
 
 # Delete old files
 rm $FIL
